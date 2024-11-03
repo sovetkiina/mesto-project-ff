a13b463b4015f21167ceac2468fca127702b6e85
sovetkiina <sovetkiina@gmail.com>
2024-11-03 23:55:45 +0100

    Добавлена функция renderCards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,36 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardListContainer = document.querySelector(".places__list");
+
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove(); 
+}
+
+function renderCards(cardArray) {
+  cardArray.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    cardListContainer.append(cardElement);
+  });
+}
+
+renderCards(initialCards);
